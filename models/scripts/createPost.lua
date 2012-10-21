552a454e755f00e69a47474b7f4ee6070efc7668
Henrik Joreteg <hjoreteg@gmail.com>
2012-10-21 11:01:38 -0700

    emptying scripts for edumacationalization purposes

--- models/scripts/createPost.lua
+++ models/scripts/createPost.lua
@@ -1,19 +1,1 @@
 local author, title, body, time = unpack(ARGV);
-
-if #author == 0 or #title == 0 or #body == 0 then
-    return {'Must have an author, title and body'};
-end
-
--- get a new id
-local id = redis.call('incr', 'posts:cid');
-
-redis.call('SET', 'postattributes:'..id, cjson.encode({
-    author = author, 
-    title = title, 
-    body = body,
-    id = id
-}));
-
-redis.call('ZADD', 'posts', time, id);
-
-return {false, {id = id, success = true}};
